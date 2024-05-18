0caefbc1943f0757199002cac6f0bf6598fbec65
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-05-18 22:24:22 +0530

    Array

---
+++ 04.Array/array.js
@@ -0,0 +1,15 @@
+var superGeros = [];
+var heroPower = [];
+var arrays = [];
+superGeros.push("spiderman");
+heroPower.push(521);
+console.log(heroPower);
+console.log(superGeros);
+var allUsers = [];
+allUsers.push({ name: "Sagar", isActive: true });
+console.log(allUsers);
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+console.log(MLModels);
---
+++ 04.Array/array.ts
@@ -0,0 +1,25 @@
+const superGeros:string[] =[]
+const heroPower :number[] = []
+const arrays:Array<number> = []
+
+superGeros.push("spiderman")
+heroPower.push(521)
+console.log(heroPower)
+console.log(superGeros)
+
+type User ={
+    name:string
+    isActive:boolean
+}
+
+const allUsers:User[] = []
+
+allUsers.push({name:"Sagar",isActive:true})
+console.log(allUsers)
+
+const MLModels: number[][] = [
+    [255,255,255],
+    []
+]
+
+console.log(MLModels)
