cfaaff1bccaad00fad92806dc97fb7900e73a162
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-05-18 19:54:09 +0530

    type variable

--- 03.Objects/myObject.js
+++ 03.Objects/myObject.js
@@ -1,3 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var User = {
     name: "Sagar",
     email: "sagar@gmaiul.com",
@@ -6,4 +8,21 @@ var User = {
 function creteUser(_a) {
     var string = _a.name, boolean = _a.isPaid;
 }
-creteUser();
+var newUser = { name: "sagar", isPaid: true };
+creteUser(newUser);
+function createCourse() {
+    return { name: "reactJs", price: 399 };
+}
+function createNewUser(User) {
+    return { name: "", email: "", isActive: true };
+}
+createNewUser({ name: "", email: "", isActive: true });
+function createUserNew(U) {
+}
+var myUser = {
+    _id: "12345",
+    name: "S",
+    email: "S@gmail.com",
+    isActive: false,
+};
+myUser.email = "SAgar";
--- 03.Objects/myObject.ts
+++ 03.Objects/myObject.ts
@@ -15,4 +15,37 @@ function createCourse():{name:string,price:number}{
     return {name:"reactJs",price:399}
 }
 
+ type User = {
+    name :string,
+    email:string,
+    isActive:boolean
+ }
+
+ type myString = string
+ function createNewUser(User:User){
+    return {name:"",email:"",isActive:true}
+ }
+ createNewUser({name:"",email:"",isActive:true});
+
+ type UserIdCreate = {
+    readonly _id : string,
+    name :string,
+    email:string,
+    isActive:boolean
+    createDeatils?:number
+ }
+
+ function createUserNew(U:UserIdCreate){
+
+ }
+ let myUser : UserIdCreate = {
+    _id:"12345",
+    name:"S",
+    email:"S@gmail.com",
+    isActive:false,
+   
+ }
+
+ myUser.email = "SAgar";
+
 export{}
